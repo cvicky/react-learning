99c6cbc0d9dc394fd907cae7eadd9dba259e5e3a
Vicky Chen <c.vicky1213@gmail.com>
2017-07-21 17:25:29 -0400

    add components

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css');
 
 //state
@@ -8,19 +9,42 @@ require('./index.css');
 
 
 
-class App extends React.Component{
+class Banner extends React.Component{
 	render(){
 		return(
 			<div> 
-				Hello World!
+				<h1> 10 out of 2500 activated </h1>
 			</div>
 		)
 	}
+}
 
+class Users extends React.Component{
+	render(){
+		return(
+			<div>
+				<h4>Name  Email	Date Signed Up </h4>
+				<ul>
+					<li>Beyonce yonce@4.com 4/4/4 </li>
+					<li> JT		JT@jt.com 	5/1/17 </li>
+				</ul> 
+			</div>
+		)
+	}
+}
+
+class Both extends React.Component{
+	render(){
+		return(
+			<div>
+				<Banner/>
+				<Users/>
+			</div>
+			)
+	}
 }
 
 
 ReactDOM.render(
-	<App/>,
-	document.getElementById('app')
+	<Both/>
 	)
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 // In webpack.config.js
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin'); 
+
 module.exports = {
   entry: './app/index.js',
   module: {
     rules: [
-      { test: /\.coffee$/, use: "coffee-loader" }
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ['style-loader', 'css-loader']}
     ]
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js'
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  ]
+  plugins: [new HtmlWebpackPlugin({
+    template: 'app/index.html'
+  })]
 }
